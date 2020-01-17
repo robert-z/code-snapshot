40989f7761c46cb6ffc40b3c338f7be9e9e1c79a
robert-z <robertz.perez1985@gmail.com>
2020-01-17 11:26:09 +0100

    Create and show a new webview panel

--- src/extension.ts
+++ src/extension.ts
@@ -5,10 +5,28 @@ export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "code-snapshot" is now active!');
 
 	let disposableCodeSnapshotInit = vscode.commands.registerCommand('extension.code.snapshot.init', () => {
-		vscode.window.showInformationMessage('Hello World!');
+
+		showPanel();
+		
+		// vscode.window.showInformationMessage('Hello World!');
 	});
 
 	context.subscriptions.push(disposableCodeSnapshotInit);
 }
 
 export function deactivate() {}
+
+function showPanel() {
+	let panel: vscode.WebviewPanel;
+
+	panel = vscode.window.createWebviewPanel(
+		'codeSnapshot',
+		'Code Snapshot',
+		vscode.ViewColumn.Two,
+		{
+			enableScripts: true,
+		}
+	);
+	
+	panel.webview.html = '<h1>Hello World!</h1>';
+}
