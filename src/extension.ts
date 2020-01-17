ee3b2fa2a5cc549dbb71fe22e6df36355e53a212
robert-z <robertz.perez1985@gmail.com>
2020-01-17 13:22:44 +0100

    Load template from file

--- src/extension.ts
+++ src/extension.ts
@@ -1,32 +1,28 @@
 import * as vscode from 'vscode';
+import * as path from 'path';
+import * as fs from 'fs';
+
+const VIEW_TYPE = 'codeSnapshot';
+const WEB_VIEW_TITLE = 'Code Snapshot';
 
 export function activate(context: vscode.ExtensionContext) {
 
 	console.log('Congratulations, your extension "code-snapshot" is now active!');
 
-	let disposableCodeSnapshotInit = vscode.commands.registerCommand('extension.code.snapshot.init', () => {
+	const htmlTemplatePath = path.resolve(context.extensionPath, "src/template/index.html");
+	let panel: vscode.WebviewPanel;
 
-		showPanel();
+	let disposableCodeSnapshotInit = vscode.commands.registerCommand('extension.code.snapshot.init', () => {
+		panel = vscode.window.createWebviewPanel(
+			VIEW_TYPE,
+			WEB_VIEW_TITLE,
+			vscode.ViewColumn.Two
+		);
 		
-		// vscode.window.showInformationMessage('Hello World!');
+		panel.webview.html = fs.readFileSync(htmlTemplatePath, "utf-8");
 	});
 
 	context.subscriptions.push(disposableCodeSnapshotInit);
 }
 
 export function deactivate() {}
-
-function showPanel() {
-	let panel: vscode.WebviewPanel;
-
-	panel = vscode.window.createWebviewPanel(
-		'codeSnapshot',
-		'Code Snapshot',
-		vscode.ViewColumn.Two,
-		{
-			enableScripts: true,
-		}
-	);
-	
-	panel.webview.html = '<h1>Hello World!</h1>';
-}
