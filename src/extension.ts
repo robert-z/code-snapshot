96abf98ca979f9dc9afdc9efeda7ada89a46f782
robert-z <robertz.perez1985@gmail.com>
2020-02-02 01:06:31 +0100

    Migrate from TSLint to ESLint

--- src/extension.ts
+++ src/extension.ts
@@ -6,65 +6,68 @@ const VIEW_TYPE = 'codeSnapshot';
 const WEB_VIEW_TITLE = 'Code Snapshot';
 
 const init = (context: vscode.ExtensionContext) => {
-	const activeTextEditor = vscode.window.activeTextEditor;
+    const activeTextEditor = vscode.window.activeTextEditor;
 
-	const panel = createPanel(context);
+    const panel = createPanel(context);
 
-	const selectionHandler = vscode.window.onDidChangeTextEditorSelection(e => {
-		if (hasTextSelected(e.textEditor.selection)) {
-			update(panel)
-		}
-	});
+    const selectionHandler = vscode.window.onDidChangeTextEditorSelection(e => {
+        if (hasTextSelected(e.textEditor.selection)) {
+            update(panel);
+        }
+    });
 
-	panel.onDidDispose(() => selectionHandler.dispose());
+    panel.onDidDispose(() => selectionHandler.dispose());
 
-	if (hasTextSelected(activeTextEditor?.selection)) update(panel);
-}
-
-const createPanel = (context: vscode.ExtensionContext) : vscode.WebviewPanel => {
-	const htmlTemplatePath = path.resolve(context.extensionPath, "webview/index.html");
-	const iconPath = path.resolve(context.extensionPath, "webview/assets/images/icon-label.png");
-	const panel = vscode.window.createWebviewPanel(
-		VIEW_TYPE,
-		WEB_VIEW_TITLE,
-		vscode.ViewColumn.Two,
-		{
-			enableScripts: true,
-			localResourceRoots: [vscode.Uri.file(context.extensionPath)]
-		}
-	);
-	
-	panel.iconPath = vscode.Uri.file(iconPath);
-
-	panel.webview.html = getTemplate(htmlTemplatePath);
-
-	return panel;
+    if (hasTextSelected(activeTextEditor?.selection)) update(panel);
 };
 
-const getTemplate = (htmlTemplatePath:string): string => {
-	const htmlContent = fs.readFileSync(htmlTemplatePath, "utf-8");
-	return htmlContent.replace(/(src|href)="([^"]*)"/gu, (_, match, src) => {
-		let assetsPath = vscode.Uri.file(path.resolve(htmlTemplatePath, '..', src)).with({
-			scheme: "vscode-resource"
-		}).toString();
-		return `${match}="${assetsPath}"`
-	})
-}
+const createPanel = (context: vscode.ExtensionContext): vscode.WebviewPanel => {
+    const htmlTemplatePath = path.resolve(context.extensionPath, 'webview/index.html');
+    const iconPath = path.resolve(context.extensionPath, 'webview/assets/images/icon-label.png');
+    const panel = vscode.window.createWebviewPanel(
+        VIEW_TYPE,
+        WEB_VIEW_TITLE,
+        vscode.ViewColumn.Two,
+        {
+            enableScripts: true,
+            localResourceRoots: [vscode.Uri.file(context.extensionPath)]
+        }
+    );
 
-const update = (panel:vscode.WebviewPanel): void => {
-	vscode.commands.executeCommand('editor.action.clipboardCopyAction');
-	
-	panel.webview.postMessage({
-		type: 'updateCode',
-	});
+    panel.iconPath = vscode.Uri.file(iconPath);
+
+    panel.webview.html = getTemplate(htmlTemplatePath);
+
+    return panel;
 };
 
-const hasTextSelected = (selection:vscode.Selection | undefined): Boolean => !!selection && !selection.isEmpty;
+const getTemplate = (htmlTemplatePath: string): string => {
+    const htmlContent = fs.readFileSync(htmlTemplatePath, 'utf-8');
+    return htmlContent.replace(/(src|href)="([^"]*)"/gu, (_, match, src) => {
+        let assetsPath = vscode.Uri.file(path.resolve(htmlTemplatePath, '..', src))
+            .with({
+                scheme: 'vscode-resource'
+            })
+            .toString();
+        return `${match}="${assetsPath}"`;
+    });
+};
+
+const update = (panel: vscode.WebviewPanel): void => {
+    vscode.commands.executeCommand('editor.action.clipboardCopyAction');
+
+    panel.webview.postMessage({
+        type: 'updateCode'
+    });
+};
+
+const hasTextSelected = (selection: vscode.Selection | undefined): Boolean =>
+    !!selection && !selection.isEmpty;
 
 export const activate = (context: vscode.ExtensionContext) => {
-	return context.subscriptions.push(
-		vscode.commands.registerCommand('codesnapshot.init', () => init(context))
-	);
-}
+    return context.subscriptions.push(
+        vscode.commands.registerCommand('codesnapshot.init', () => init(context))
+    );
+};
 
-export const deactivate = () => {}
+export const deactivate = () => {};
--- webview/assets/js/main.js
+++ webview/assets/js/main.js
@@ -1,22 +1,24 @@
 (() => {
-    const snapshotContainerNode             = document.querySelector('.snapshot-container');
-    const snapshotContainerBackgroundNode   = document.querySelector('.snapshot-container__background');
-    const terminalNode                      = document.querySelector('.terminal');
-    const terminalCodeSnippetNode           = document.querySelector('.terminal__code-snippet');
-    const sizeNode                          = document.querySelector('.header__size');
-    const shootNode                         = document.querySelector('.shoot');
+    const snapshotContainerNode = document.querySelector('.snapshot-container');
+    const snapshotContainerBackgroundNode = document.querySelector(
+        '.snapshot-container__background'
+    );
+    const terminalNode = document.querySelector('.terminal');
+    const terminalCodeSnippetNode = document.querySelector('.terminal__code-snippet');
+    const sizeNode = document.querySelector('.header__size');
+    const shootNode = document.querySelector('.shoot');
 
     const pasteCode = (clip, element) => {
         const code = getHtml(clip);
         element.innerHTML = code;
         setupTerminal(element);
-    }
+    };
 
-    const getHtml = clip => clip.getData('text/html')
+    const getHtml = clip => clip.getData('text/html');
 
     const setupTerminal = node => {
         node.innerHTML = replaceBrByDiv(node.innerHTML);
-        
+
         const lines = node.querySelectorAll('div > div');
 
         lines.forEach((row, index) => {
@@ -31,7 +33,7 @@
 
         document.body.style.setProperty('--line-number-width', width + 'px');
         document.body.style.setProperty('--editor-line-padding-left', width + 15 + 'px');
-    }
+    };
 
     const computeEdeditorLineNumberWidth = text => {
         const div = document.body.appendChild(document.createElement('div'));
@@ -40,51 +42,52 @@
         const width = div.clientWidth;
         div.remove();
         return width;
-    }
+    };
 
-    const replaceBrByDiv = (str) => {
-        return str.replace( /(<br>)/ig, '<div>&nbsp;</div>');
-    }
+    const replaceBrByDiv = str => {
+        return str.replace(/(<br>)/gi, '<div>&nbsp;</div>');
+    };
 
     window.addEventListener('message', ({ data: { type } }) => {
-        switch(type) {
+        switch (type) {
             case 'updateCode':
-                document.execCommand('paste')
-            break;
+                document.execCommand('paste');
+                break;
         }
     });
-    
+
     document.addEventListener('paste', event => {
         pasteCode(event.clipboardData, terminalCodeSnippetNode);
     });
-    
+
     shootNode.addEventListener('click', e => {
         snapshotContainerNode.style.resize = 'none';
         terminalNode.style.resize = 'none';
-        
-        domtoimage.toBlob(snapshotContainerBackgroundNode, {
-            width: snapshotContainerBackgroundNode.offsetWidth * 2,
-            height: snapshotContainerBackgroundNode.offsetHeight * 2,
-            style: {
-                'transform': 'scale(2)',
-                'transform-origin': 'center',
-                'background': '#e0eafc',
-                'background': 'linear-gradient(to left, #e0eafc, #cfdef3);'
-            }
-        })
-        .then(function (blob) {
-            snapshotContainerNode.style.resize = '',
-            terminalNode.style.resize = '',
-            window.saveAs(blob, 'code-snapshot.png');
-        });
+
+        domtoimage
+            .toBlob(snapshotContainerBackgroundNode, {
+                width: snapshotContainerBackgroundNode.offsetWidth * 2,
+                height: snapshotContainerBackgroundNode.offsetHeight * 2,
+                style: {
+                    transform: 'scale(2)',
+                    'transform-origin': 'center',
+                    background: '#e0eafc',
+                    background: 'linear-gradient(to left, #e0eafc, #cfdef3);'
+                }
+            })
+            .then(function(blob) {
+                (snapshotContainerNode.style.resize = ''),
+                    (terminalNode.style.resize = ''),
+                    window.saveAs(blob, 'code-snapshot.png');
+            });
     });
 
-    window.addEventListener('colorPickerChange', function (data) {
+    window.addEventListener('colorPickerChange', function(data) {
         let i = data.detail.el;
         snapshotContainerBackgroundNode.style.backgroundColor = i.value;
     });
 
-    colorPicker.initAll()
+    colorPicker.initAll();
 
     if (ResizeObserver) {
         const resizeObserver = new ResizeObserver(entries => {
@@ -93,7 +96,7 @@
             let w = Math.round(snapshotContainerNode.offsetWidth) * 2;
             let h = Math.round(snapshotContainerNode.offsetHeight) * 2;
 
-            sizeNode.textContent = w + "x" + h;
+            sizeNode.textContent = w + 'x' + h;
         });
 
         resizeObserver.observe(snapshotContainerNode);
