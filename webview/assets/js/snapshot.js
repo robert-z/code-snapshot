584aa173f9f0de88f826020cf8efc2a0066e6c44
Tim McCabe <me@timmcca.be>
2021-03-15 12:03:44 -0400

    Add option to export SVG screenshot

--- webview/assets/js/snapshot.js
+++ webview/assets/js/snapshot.js
@@ -1,25 +1,44 @@
 const snapshotContainerNode = document.querySelector('.snapshot-container');
 const snapshotContainerBackgroundNode = document.querySelector('.snapshot-container__background');
 const terminalNode = document.querySelector('.terminal');
+const exportSvgNode = document.getElementById('export-svg');
+
+const resetStyles = () => {
+    snapshotContainerNode.style.resize = '';
+    terminalNode.style.resize = '';
+}
 
 export const takeSnapshot = () => {
     snapshotContainerNode.style.resize = 'none';
     terminalNode.style.resize = 'none';
 
-    domtoimage
-        .toBlob(snapshotContainerBackgroundNode, {
-            width: snapshotContainerBackgroundNode.offsetWidth * 2,
-            height: snapshotContainerBackgroundNode.offsetHeight * 2,
-            style: {
-                transform: 'scale(2)',
-                'transform-origin': 'center',
-                background: '#e0eafc',
-                background: 'linear-gradient(to left, #e0eafc, #cfdef3);'
-            }
-        })
-        .then(function(blob) {
-            snapshotContainerNode.style.resize = '';
-            terminalNode.style.resize = '';
-            window.saveAs(blob, 'code-snapshot.png');
-        });
+    const options = {
+        width: snapshotContainerBackgroundNode.offsetWidth * 2,
+        height: snapshotContainerBackgroundNode.offsetHeight * 2,
+        style: {
+            transform: 'scale(2)',
+            'transform-origin': 'center',
+            background: '#e0eafc',
+            background: 'linear-gradient(to left, #e0eafc, #cfdef3);'
+        }
+    };
+
+    if(exportSvgNode.checked) {
+        domtoimage
+            .toSvg(snapshotContainerBackgroundNode, options)
+            .then(function(dataUrl) {
+                resetStyles();
+                var link = document.createElement('a');
+                link.download = 'code-snapshot.svg';
+                link.href = dataUrl;
+                link.click();
+            });
+    } else {
+        domtoimage
+            .toBlob(snapshotContainerBackgroundNode, options)
+            .then(function(blob) {
+                resetStyles();
+                window.saveAs(blob, 'code-snapshot.png');
+            });
+    }
 };
